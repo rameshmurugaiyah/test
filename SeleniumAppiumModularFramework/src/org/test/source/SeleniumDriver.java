7148404fc60a4749234f1bb9e5bc554b6402c8c6
rameshmurugaiyah <be2005.ramesh@gmail.com>
2015-08-11 19:45:32 +0530

    Update SeleniumDriver.java

--- SeleniumAppiumModularFramework/src/org/test/source/SeleniumDriver.java
+++ SeleniumAppiumModularFramework/src/org/test/source/SeleniumDriver.java
@@ -3,7 +3,6 @@ package org.test.source;
 public class SeleniumDriver {
 	public void display(){
 		System.out.println("Hello World");
-		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
 	}
 }
